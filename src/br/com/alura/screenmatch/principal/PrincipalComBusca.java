f73cff6ae52dcee9678f3d057d906306868719cb
F2872541 Elton Rodrigues Homem <elton.homem@bb.com.br>
2024-10-03 09:09:28 -0300

    busca com scanner

--- src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -5,15 +5,21 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class PrincipalComBusca {
     public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner leitura = new Scanner(System.in);
+        System.out.println("Digite um filme para a busca: ");
+        var busca = leitura.nextLine();
+        String endereco = String.format("http://www.omdbapi.com/?t=%s&apikey=3d4269d4", busca);
+        //String endereco = "http://www.omdbapi.com/?t=" + busca + "&apikey=3d4269d4";
+
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("http://www.omdbapi.com/?t=matrix&apikey=3d4269d4"))
+                .uri(URI.create(endereco))
                 .build();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
         System.out.println(response.body());
     }
-
 }
