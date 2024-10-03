a7d9b2148896c2fae98b1a2bb5afe3183206e2d1
F2872541 Elton Rodrigues Homem <elton.homem@bb.com.br>
2024-10-03 11:20:21 -0300

    Gson classe e Serialize

--- src/br/com/alura/screenmatch/principal/MealDBAPI.java
+++ src/br/com/alura/screenmatch/principal/MealDBAPI.java
@@ -1,5 +1,7 @@
 package br.com.alura.screenmatch.principal;
 
+import com.google.gson.Gson;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -23,6 +25,7 @@ public class MealDBAPI {
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
         System.out.println(response.body());
+
 
 
 
--- src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -1,11 +1,14 @@
 package br.com.alura.screenmatch.principal;
 
+import com.google.gson.Gson;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.Scanner;
+
+import br.com.alura.screenmatch.modelos.Titulo;
 
 public class PrincipalComBusca {
     public static void main(String[] args) throws IOException, InterruptedException {
@@ -20,6 +23,13 @@ public class PrincipalComBusca {
                 .uri(URI.create(endereco))
                 .build();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println(response.body());
+
+        String json = response.body();
+        System.out.println(json);
+
+        Gson gson = new Gson();
+        Titulo meuTitulo = gson.fromJson(json, Titulo.class);
+        System.out.println(meuTitulo);
+
     }
 }
